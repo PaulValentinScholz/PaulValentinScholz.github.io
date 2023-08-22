6802207deee868c015c28ed8f64c217c758cd22f
PaulValentinScholz <me@pavasch.com>
2023-08-22 18:43:13 +0200

    Add Contact.html and Partners.html

--- script/main.js
+++ script/main.js
@@ -3,19 +3,47 @@ const darkModeBtn = document.getElementById('dark-mode-btn');
 const dropdownToggleIcon = document.querySelector('.btn.dropdown-toggle i');
 const body = document.body;
 
+// Check if there's a stored theme preference in local storage
+const storedTheme = localStorage.getItem('theme');
+
+// Set the initial theme based on the stored preference or default to 'light'
+if (storedTheme === 'dark') {
+    body.setAttribute('data-bs-theme', 'dark');
+    dropdownToggleIcon.className = 'bi bi-sun-fill';
+    darkModeBtn.classList.add('active');
+} else {
+    body.setAttribute('data-bs-theme', 'light');
+    dropdownToggleIcon.className = 'bi bi-moon-stars-fill';
+    lightModeBtn.classList.add('active');
+}
+
 lightModeBtn.addEventListener('click', () => {
     body.setAttribute('data-bs-theme', 'light');
-    // Change the icon in the dropdown toggler button
     dropdownToggleIcon.className = 'bi bi-moon-stars-fill';
-    lightModeBtn.classList.add("active")
-    darkModeBtn.classList.remove("active")
+    lightModeBtn.classList.add('active');
+    darkModeBtn.classList.remove('active');
+    // Store the theme preference in local storage
+    localStorage.setItem('theme', 'light');
 });
 
 darkModeBtn.addEventListener('click', () => {
     body.setAttribute('data-bs-theme', 'dark');
-    // Change the icon in the dropdown toggler button
     dropdownToggleIcon.className = 'bi bi-sun-fill';
-    darkModeBtn.classList.add("active")
-    lightModeBtn.classList.remove("active")
+    darkModeBtn.classList.add('active');
+    lightModeBtn.classList.remove('active');
+    // Store the theme preference in local storage
+    localStorage.setItem('theme', 'dark');
+});
 
+
+//Marquee
+
+const marqueeItems = document.querySelector('.marquee-items');
+
+marqueeItems.addEventListener('animationiteration', () => {
+    // When the animation iteration restarts, move the last item to the beginning
+    const items = document.querySelectorAll('.marquee-item');
+    const lastItem = items[items.length - 1];
+    marqueeItems.removeChild(lastItem);
+    marqueeItems.insertBefore(lastItem, items[0]);
 });
